960f37380daaef0c4f5957ba3f412b90330e58e2
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-01 17:22:28 +0200

    Initial commit. Added method for getting the userId

---
+++ RazorPagesUI/Utility/IdentityUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RazorPagesUI.Utility
+{
+    public static class IdentityUtility
+    {
+        public static string GetUserId(ClaimsIdentity claimsIdentity)
+        {
+            return claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}
