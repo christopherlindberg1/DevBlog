5e0232095e94e203d4a078f2497e7953c2393740
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-01 21:35:25 +0200

    Implemented method for getting a blog post by Id

--- DataAccess/Data/BlogPostData.cs
+++ DataAccess/Data/BlogPostData.cs
@@ -28,11 +28,35 @@ namespace DataAccess.Data
         /// Gets all blog posts
         /// </summary>
         /// <returns>List containing all blog posts</returns>
-        public async Task<List<BlogPostModel>> GetAllBlogPosts()
+        public async Task<List<BlogPostModel>> GetAllBlogPostsOrderByDateDesc()
         {
             return await _dataAccess.LoadData<BlogPostModel, dynamic>(
                 "dbo.spBlogPost_GetAllOrderByDateDesc",
                 new { },
+                _connectionStringData.SqlConnectionName);
+        }
+
+        public async Task<BlogPostModel> GetById(int id)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("Id", id);
+
+            IEnumerable<BlogPostModel> rows = await _dataAccess.LoadData<BlogPostModel, dynamic>(
+                "dbo.spBlogPost_GetById",
+                parameters,
+                _connectionStringData.SqlConnectionName);
+
+            return rows.FirstOrDefault();
+        }
+
+        public async Task<List<BlogPostModel>> GetCurrentUsersBlogPostsOrderByDateDesc(string userId)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("AuthorId", userId);
+
+            return await _dataAccess.LoadData<BlogPostModel, dynamic>(
+                "dbo.spBlogPost_GetCurrentUsersBlogPostsOrderByDateDesc",
+                parameters,
                 _connectionStringData.SqlConnectionName);
         }
 
