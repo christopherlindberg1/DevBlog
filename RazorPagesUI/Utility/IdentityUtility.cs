98d722d1533c9746a7a4e57094a1a9284d16fbf0
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-01 21:34:24 +0200

    added comment

--- RazorPagesUI/Utility/IdentityUtility.cs
+++ RazorPagesUI/Utility/IdentityUtility.cs
@@ -8,6 +8,11 @@ namespace RazorPagesUI.Utility
 {
     public static class IdentityUtility
     {
+        /// <summary>
+        /// Returns the user Id of the current user.
+        /// </summary>
+        /// <param name="claimsIdentity">ClaimsItentity passed in by the client.</param>
+        /// <returns>user id as a string.</returns>
         public static string GetUserId(ClaimsIdentity claimsIdentity)
         {
             return claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
