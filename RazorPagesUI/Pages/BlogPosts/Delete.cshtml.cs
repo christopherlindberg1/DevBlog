cbc27f12a81ab117e343fde2c2dae1c190e7f276
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-02 01:24:56 +0200

    Implemented backend logic for get request

---
+++ RazorPagesUI/Pages/BlogPosts/Delete.cshtml.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DataAccess.Data;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RazorPagesUI.Utility;
+
+namespace RazorPagesUI.Pages.BlogPosts
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ILogger<DeleteModel> _logger;
+        private readonly IBlogPostData _blogPostData;
+
+        [BindProperty]
+        public BlogPostModel BlogPost { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int BlogPostId { get; set; }
+
+        public string UserId { get; set; } = null;
+
+        public DeleteModel(ILogger<DeleteModel> logger, IBlogPostData blogPostData)
+        {
+            _logger = logger;
+            _blogPostData = blogPostData;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            UserId = IdentityUtility.GetUserId((ClaimsIdentity)User.Identity);
+
+            BlogPost = await _blogPostData.GetById(id);
+
+            if (BlogPost == null)
+            {
+                // Fix better user experience than this
+                return RedirectToPage("./Index");
+            }
+
+            if (UserId != BlogPost.AuthorId)
+            {
+                return RedirectToPage("./Index");
+            }
+
+            BlogPostId = id;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            UserId = IdentityUtility.GetUserId((ClaimsIdentity)User.Identity);
+
+            BlogPost = await _blogPostData.GetById(BlogPostId);
+
+            if (BlogPost == null)
+            {
+                // Fix better user experience than this
+                return RedirectToPage("./Index");
+            }
+
+            if (UserId != BlogPost.AuthorId)
+            {
+                return RedirectToPage("./Index");
+            }
+
+            await _blogPostData.DeleteBlogPost(BlogPostId);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
