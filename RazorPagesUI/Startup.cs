e84796abdd00f0b0a3a954182e9f0ab1b8348dc7
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-04 22:11:41 +0200

    Configured EF to use custom user- and role classes instead of the default ones

--- RazorPagesUI/Startup.cs
+++ RazorPagesUI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RazorPagesUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,8 +34,13 @@ namespace BlogRazorPages
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddRazorPages();
 
             // Services needed to access the database with Dapper
