93844e221342a1f44db2fd2b945052a332d8709c
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-04 14:43:57 +0200

    model for holding data when adding a comment

---
+++ DataAccess/Models/BlogPostAddCommentModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class BlogPostAddCommentModel
+    {
+        [Required]
+        public int BlogPostId { get; set; }
+
+        [Required]
+        public string AuthorId { get; set; }
+
+        [Required]
+        [Display(Name = "Comment Text")]
+        public string CommentText { get; set; }
+    }
+}
