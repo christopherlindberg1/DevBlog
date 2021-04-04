9f198d4d23e3c15f75c642c7e81f5a903c219d4d
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-04 22:12:20 +0200

    Added MaxLength of 50 to the properties FirstName and LastName

--- RazorPagesUI/Models/ApplicationUser.cs
+++ RazorPagesUI/Models/ApplicationUser.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BlogRazorPages.Models
+namespace RazorPagesUI.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [MaxLength(50, ErrorMessage = "First name cannot be more than 50 characters")]
         public string FirstName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Last name cannot be more than 50 characters")]
         public string LastName { get; set; }
     }
 }
