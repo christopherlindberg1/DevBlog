7e719812f24816eeb0877b4711c40df7b4534a40
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-23 19:19:10 +0200

    Added property DisplayUserName

--- RazorPagesUI/Models/ApplicationUser.cs
+++ RazorPagesUI/Models/ApplicationUser.cs
@@ -9,10 +9,15 @@ namespace RazorPagesUI.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        [MaxLength(50, ErrorMessage = "First name cannot be more than 50 characters")]
+        [MaxLength(50, ErrorMessage = "First name cannot be more than 50 characters.")]
         public string FirstName { get; set; }
 
-        [MaxLength(50, ErrorMessage = "Last name cannot be more than 50 characters")]
+        [MaxLength(50, ErrorMessage = "Last name cannot be more than 50 characters.")]
         public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(30, ErrorMessage = "User name can not be more than 30 characters.")]
+        [MinLength(30, ErrorMessage = "User name must be at least 3 characters.")]
+        public string DisplayUserName { get; set; }
     }
 }
