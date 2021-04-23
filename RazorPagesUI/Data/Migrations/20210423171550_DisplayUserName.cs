26c712b504c3b153c919ff16fff40258e4203f88
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-23 19:18:17 +0200

    Added migration for adding the column DisplayUserName to the AspNetUsers table

---
+++ RazorPagesUI/Data/Migrations/20210423171550_DisplayUserName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogRazorPages.Data.Migrations
+{
+    public partial class DisplayUserName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayUserName",
+                table: "AspNetUsers",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayUserName",
+                table: "AspNetUsers");
+        }
+    }
+}
